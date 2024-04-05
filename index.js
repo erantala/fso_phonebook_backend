d41c1ff48b4e1dacc1dd766643a3d9a4bd8f4e2b
Eero Rantala <eero.rantala@nitor.com>
2024-04-05 20:54:21 +0300

    Puhelinluettelon backend step6

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const app = express()
 
-const persons = [
+let persons = [
     {
         name: "Arto Hellas",
         number: "040-123456",
@@ -29,8 +29,73 @@ const persons = [
     }
 ]
 
+app.use(express.json())
+
+const infoPage = () => {
+    return (`<p>Phonebook has info for ${persons.length} people</p>
+    <p>${new Date()}</p>`)
+}
+
+const generateRandomId = () => {
+    return Math.floor(Math.random() * Number.MAX_SAFE_INTEGER)
+}
+
 app.get('/api/persons', (request, response) => {
     response.json(persons)
+})
+
+app.get('/api/persons/:id', (request, response) => {
+    const id = Number(request.params.id)
+    const person = persons.find(person => person.id === id)
+
+    if (person) {
+        response.json(person)
+    } else {
+        response.status(404).end()
+    }
+})
+
+app.delete('/api/persons/:id', (request, response) => {
+    const id = Number(request.params.id)
+    persons = persons.filter(person => person.id !== id)
+
+    response.status(204).end()
+})
+
+const validatePerson = (request, response, next) => {
+    console.log('validatePerson')
+    console.log(request.body)
+    const reqBody = request.body
+    let errorMsg = null
+
+    if (!reqBody.name) {
+        errorMsg = 'name is missing'
+    } else if (!reqBody.number) {
+        errorMsg = 'number is missing'
+    } else if (persons.find(person => person.name === reqBody.name)) {
+        errorMsg = 'name must be unique'
+    }
+
+    if (errorMsg) {
+        return response.status(400).json({ error: errorMsg })
+    }
+    next()
+}
+
+app.post('/api/persons/', validatePerson, (request, response) => {
+    console.log('validatePerson passed')
+    const reqBody = request.body
+    const newPerson = {
+        name: reqBody.name,
+        number: reqBody.number,
+        id: generateRandomId()
+    }
+    persons.push(newPerson)
+    response.json(newPerson)
+})
+
+app.get('/info', (request, response) => {
+    response.send(infoPage())
 })
 
 const PORT = 3001
